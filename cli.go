44115a21ac58e3b8dc75f1d4e1e99efce3816fc5
mokrz <mokrz@github.com>
2021-05-27 10:29:10 -0700

    I/O helpers

--- cli.go
+++ cli.go
@@ -2,24 +2,29 @@ package cli
 
 import (
 	"flag"
+	"io"
+	"os"
 )
 
 type HandleFn func(cmd *Command, args []string) error
 
 type Command struct {
-	Name     string
-	Handler  HandleFn
-	Flags    *flag.FlagSet
-	children map[string]*Command
-	parent *Command
+	Name           string
+	Handler        HandleFn
+	Flags          *flag.FlagSet
+	children       map[string]*Command
+	parent         *Command
+	stdout, stderr io.Writer
 }
 
 func NewCommand(name string, handler HandleFn) *Command {
 	return &Command{
-		Name:    name,
-		Handler: handler,
-		Flags: flag.NewFlagSet(name, flag.ExitOnError),
+		Name:     name,
+		Handler:  handler,
+		Flags:    flag.NewFlagSet(name, flag.ExitOnError),
 		children: make(map[string]*Command),
+		stdout:   os.Stdout,
+		stderr:   os.Stderr,
 	}
 }
 
@@ -28,7 +33,7 @@ func (c *Command) AddCommand(cmd *Command) {
 	c.children[cmd.Name].parent = c
 }
 
-func (c *Command) GetSubcommand(name string) (*Command) {
+func (c *Command) GetSubcommand(name string) *Command {
 	return c.children[name]
 }
 
@@ -42,4 +47,34 @@ func (c *Command) Execute(args []string) error {
 	}
 
 	return c.Handler(c, args)
+}
+
+func (c *Command) SetStdout(out io.Writer) {
+	c.stdout = out
+}
+
+func (c *Command) SetStderr(err io.Writer) {
+	c.stderr = err
+}
+
+func (c *Command) Stdout() io.Writer {
+
+	if c.stdout != nil {
+		return c.stdout
+	} else if c.parent != nil {
+		return c.parent.Stdout()
+	}
+
+	return nil
+}
+
+func (c *Command) Stderr() io.Writer {
+
+	if c.stderr != nil {
+		return c.stderr
+	} else if c.parent != nil {
+		return c.parent.Stderr()
+	}
+
+	return nil
 }
